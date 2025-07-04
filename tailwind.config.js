5396b04ab91b291f83779211292fa82acf302865
Aaditya Kumawat <aaditya03kumawat@gmail.com>
2025-07-04 23:51:53 +0530

    Co-authored-by: Aakash Gupta <aakash0101200@users.noreply.github.com>

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./homepage/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        aliceblue: '#f0f8ff',
+        white: '#ffffff',
+        blue: '#1e90ff',
+      },
+    },
+  },
+  plugins: [],
+}
